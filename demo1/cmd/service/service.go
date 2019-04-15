29625c9891dd8e33c4ab836cd8820af101179872
Anthony Qu <king19800105@gmail.com>
2019-04-15 13:14:20 +0800

    demo1完成

--- demo1/cmd/service/service.go
+++ demo1/cmd/service/service.go
@@ -8,6 +8,7 @@ import (
 // 服务运行
 func Run() {
 	httpHandler := createService()
+	log.Println("demo1服务启动，服务地址：127.0.0.1:8088")
 	err := nethttp.ListenAndServe(":8088", httpHandler)
 
 	if nil != err {
